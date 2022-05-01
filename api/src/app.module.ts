417c75fea97a84a828a94b616066b4c8051b812f
zodaland <me@zodaland.com>
2022-05-01 19:12:12 +0900

    typeorm dependency 추가 및 모듈 임포트 적용

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
+
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule.register({ folder: './config' })],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: 'mysql',
+                host: configService.get('DB_HOST'),
+                port: +configService.get('DB_POST'),
+                username: configService.get('DB_USER'),
+                password: configService.get('DB_PASSWORD'),
+                database: configService.get('DB_DATABASE'),
+                entities: [__dirname + '**/*.entity(.ts}'],
+            }),
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
