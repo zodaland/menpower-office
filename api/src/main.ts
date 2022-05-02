8fecf6da184d81149302d63504986a1e5a0a1079
zodaland <me@zodaland.com>
2022-05-02 16:26:40 +0900

    들여쓰기 정리 및 global pipe 추가

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
         origin: /.*zodaland\.com$/,
         credentials: true,
-  });
-  await app.listen(3000);
+    });
+    app.useGlobalPipes(new ValidationPipe({
+        whitelist: true,
+        disableErrorMessages: process.env.NODE_ENV === 'production' ? true : false,
+    }));
+    await app.listen(3000);
 }
 bootstrap();
